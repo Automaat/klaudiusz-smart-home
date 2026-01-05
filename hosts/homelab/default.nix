4f368a51446c9309cdfb96de5cdf196937c0de1d
Marcin Skalski <skalskimarcin33@gmail.com>
2026-01-05 19:40:53 +0100

    fix(promtail): parse HA log timestamps as Europe/Warsaw time
    HA logs use local time (CET/Warsaw) but Promtail assumed UTC,
    causing Loki to reject all entries as "timestamp too new".
    
    Added location parameter to timestamp parser to correctly
    interpret log timestamps as Europe/Warsaw timezone.
    
    Fixes log ingestion errors:
    - "entry has timestamp too new: 2026-01-05T19:XX:XXZ"
    - Logs now correctly converted to UTC before sending to Loki
    
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -360,6 +360,7 @@
               timestamp = {
                 source = "timestamp";
                 format = "2006-01-02 15:04:05.000";
+                location = "Europe/Warsaw";
               };
             }
           ];
