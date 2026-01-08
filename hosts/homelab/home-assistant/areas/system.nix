4219e418b351199258b5f1c7c6dd59e843c91ee2
Marcin Skalski <skalskimarcin33@gmail.com>
2026-01-08 09:34:08 +0100

    feat(ha): add adaptive lighting sleep mode automations
    - Add manual trigger to enable sleep mode on all adaptive lights
    - Add sunrise automation to disable sleep mode
    - Affects hallway, kitchen, bathroom adaptive lighting
    
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- hosts/homelab/home-assistant/areas/system.nix
+++ hosts/homelab/home-assistant/areas/system.nix
@@ -73,5 +73,65 @@
         }
       ];
     }
+
+    # -----------------------------------------
+    # Adaptive Lighting
+    # -----------------------------------------
+    {
+      id = "adaptive_lighting_enable_sleep_mode";
+      alias = "Adaptive Lighting - Enable sleep mode all";
+      description = "Enable sleep mode on all adaptive lighting switches. Trigger manually via automation.trigger service";
+      trigger = [];
+      action = [
+        {
+          action = "adaptive_lighting.set_manual_control";
+          target.entity_id = [
+            "switch.adaptive_lighting_hallway_lights"
+            "switch.adaptive_lighting_kitchen_lights"
+            "switch.adaptive_lighting_bathroom_lights"
+          ];
+          data = {
+            manual_control = false;
+          };
+        }
+        {
+          action = "adaptive_lighting.change_switch_settings";
+          target.entity_id = [
+            "switch.adaptive_lighting_hallway_lights"
+            "switch.adaptive_lighting_kitchen_lights"
+            "switch.adaptive_lighting_bathroom_lights"
+          ];
+          data = {
+            use_defaults = "current";
+            sleep_mode_switch = true;
+          };
+        }
+      ];
+    }
+
+    {
+      id = "adaptive_lighting_disable_sleep_mode_sunrise";
+      alias = "Adaptive Lighting - Disable sleep mode on sunrise";
+      trigger = [
+        {
+          platform = "sun";
+          event = "sunrise";
+        }
+      ];
+      action = [
+        {
+          action = "adaptive_lighting.change_switch_settings";
+          target.entity_id = [
+            "switch.adaptive_lighting_hallway_lights"
+            "switch.adaptive_lighting_kitchen_lights"
+            "switch.adaptive_lighting_bathroom_lights"
+          ];
+          data = {
+            use_defaults = "current";
+            sleep_mode_switch = false;
+          };
+        }
+      ];
+    }
   ];
 }
